3618d9ddd5843c7747553e986826d6a936aa63fb
Akash Harshvardhan <92701917+Akash2624-07@users.noreply.github.com>
2026-05-04 21:49:46 +0530

    created user schema

---
+++ src/models/users.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const userSchema = new Schema({
+    firstName:{
+        type:String,
+        minLength:2,
+        maxLength:20,
+        require:true
+    },
+    lastName:{
+        type:String,
+        minLength:2,
+        maxLength:20,
+    },
+    emailId:{
+        type:String,
+        require:true,
+        unique:true,
+        lowercase:true,
+        immutable:true
+    },
+    age:{
+        type:Number,
+        min:6,
+        max:100,
+    },
+    role:{
+        type:String,
+        enum:["user", "admin"],
+        default:"user"
+    }, 
+    problemSolved:{
+        type:[String]
+    }
+})
