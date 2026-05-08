ea23af1b09da78a1e6b359e1b2ebd77257b4be3c
Akash Harshvardhan <92701917+Akash2624-07@users.noreply.github.com>
2026-05-09 04:27:38 +0530

    feat: problem schema

---
+++ src/models/problems.js
@@ -0,0 +1,73 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const problemSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    difficulty: {
+        type: String,
+        enum: ["easy", "medium", "hard"],
+        required: true
+    },
+    tags: {
+        type: [String],
+        required: true
+    },
+    visibleTestCase: [
+        {
+            input: {
+                type: String,
+                required: true
+            },
+            output: {
+                type: String,
+                required: true,
+            },
+            explanation: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    hiddenTestCase: [
+        {
+            input: {
+                type: String,
+                required: true
+            },
+            output: {
+                type: String,
+                required: true,
+            }
+        }
+    ],
+    startCode: [
+        {
+            language: {
+                type: String,
+                required: true,
+            },
+            initialCode: {
+                type: String,
+                required: true,
+            }
+        }
+    ],
+    problemCreator: {
+        type: Schema.Types.ObjectId,
+        ref: 'users',
+        required: true
+    }
+},{
+    timestamps:true
+})
+
+const Problem = mongoose.model("problems", problemSchema);
+
+module.exports = Problem;
