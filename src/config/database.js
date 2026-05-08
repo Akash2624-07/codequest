485f0ea9dc21509b7744f4fbceacfe29c385fdcc
Akash Harshvardhan <92701917+Akash2624-07@users.noreply.github.com>
2026-05-09 02:19:59 +0530

    feat: redis setup & config

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,6 @@ const mongoose = require("mongoose");
 async function Main(){
     try{
         await mongoose.connect(process.env.DB_CONNECT_URI);
-        console.log("Connected to DB successfully.");
     }
     catch(err){
         throw new Error(err.message);
--- src/index.js
+++ src/index.js
@@ -2,20 +2,23 @@ const express = require('express');
 const cookieParser = require('cookie-parser')
 require('dotenv').config();
 const Main = require('./config/database');
-const authRouter = require('./routes/userAuth')
-
-
-
+const authRouter = require('./routes/userAuth');
+const redisClient = require('./config/redis');
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+app.use("/user",authRouter);
+
+
 
 async function startServer() {
 
     try {
-        await Main();
+        // Try to connect to both Databases at once
+        await Promise.all([Main(),redisClient.connect()]);
+        console.log("Connected to DB successfully.");
 
         app.listen(process.env.PORT || 3000, () => {
             console.log("Server running on " + process.env.PORT);
