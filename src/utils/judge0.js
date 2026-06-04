dc6520d79b59f9f49730afab85e1066c51703997
Akash Harshvardhan <92701917+Akash2624-07@users.noreply.github.com>
2026-06-04 09:27:25 +0530

    feat: add submitBatch and getSubmissionResults utils for Judge0

--- src/utils/judge0.js
+++ src/utils/judge0.js
@@ -1,4 +1,6 @@
+const axios = require('axios');
 
+// Language Map 
 const LANGUAGE_MAP = {
     "cpp": 54,
     "c": 50,
@@ -17,8 +19,29 @@ const getLanguageId = (language) => {
     return id;
 }
 
+// Judge0 Client (Self-Hosted Instance)
+const judge0Client = axios.create({
+    baseURL: process.env.JUDGE0_BASE_URL,
+    headers: { "Content-Type": "application/json" }
+});
+
+// Submit batch of test cases, get back tokens 
+const submitBatch = async (submissions) => {
+    const response = await judge0Client.post(
+        "/submissions/batch?base64_encoded=false",
+        { submissions }
+    );
+    return response.data; // [{ token: "abc..." }, { token: "def..." }, ...]
+}
+
+// Token polling to get actual data
+const getSubmissionResults = async (tokens) => {
+    const tokenString = tokens.map(t => t.token).join(",");
+    const { data } = await judge0Client.get(
+        `submissions/batch?tokens=${tokenString}&base64_encoded=false`
+    );
+    return data.submissions;
+}
 
 
-
-
-module.exports = { getLanguageId };
+module.exports = { getLanguageId, submitBatch, getSubmissionResults };
