fbd68b539ccaa0cd84ec16916591b1e8e5546e2d
Akash Harshvardhan <92701917+Akash2624-07@users.noreply.github.com>
2026-06-23 02:40:39 +0530

    fix: move zod to dependencies, install @hookform/resolvers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,9 +8,9 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/login" element={<Login></Login>}></Route>
-        <Route path="/signup" element={<Signup></Signup>}></Route>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/signup" element={<Signup/>}></Route>
       </Routes>
     </>
   );
