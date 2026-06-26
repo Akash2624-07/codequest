bc398a4360d1d4edf067a6e4dc0263718a32c49b
Akash Harshvardhan <92701917+Akash2624-07@users.noreply.github.com>
2026-06-27 00:32:13 +0530

    feat(frontend): protect routes and dispatch checkAuth on app load

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,41 @@
-import { Routes, Route } from 'react-router';
+import { Routes, Route, Navigate } from 'react-router';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import { checkAuth } from './store/authSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 function App() {
+  const dispatch = useDispatch();
+  const { isAuthenticated, loading } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    dispatch(checkAuth());
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <span className="loading loading-spinner loading-lg"></span>
+      </div>
+    );
+  }
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/login" element={<Login/>}></Route>
-        <Route path="/signup" element={<Signup/>}></Route>
+        <Route
+          path="/"
+          element={isAuthenticated ? <Home /> : <Navigate to="/signup" />}
+        ></Route>
+        <Route
+          path="/login"
+          element={isAuthenticated ? <Navigate to="/" /> : <Login />}
+        ></Route>
+        <Route
+          path="/signup"
+          element={isAuthenticated ? <Navigate to="/" /> : <Signup />}
+        ></Route>
       </Routes>
     </>
   );
