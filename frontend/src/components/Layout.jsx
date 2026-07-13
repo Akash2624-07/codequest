1af40c30ce4fb13220aa9b0e0d9f89eaee2be125
Akash Harshvardhan <92701917+Akash2624-07@users.noreply.github.com>
2026-07-14 02:57:03 +0530

    feat(ui): add dark purple daisyui theme as app default

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -3,7 +3,7 @@ import Navbar from './Navbar';
 
 function Layout() {
   return (
-    <div className="min-h-screen bg-base-200 flex flex-col">
+    <div className="min-h-screen bg-base-100 flex flex-col">
       <Navbar />
       <main className="flex-1">
         <Outlet />
